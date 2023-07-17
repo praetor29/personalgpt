cb744bcb09333dc8f4f85442d1461bf9d675d198
praetor29 <pranavchip@gmail.com>
2023-07-17 21:00:26 +0530

    Function files created.

---
+++ src/main.py
@@ -0,0 +1,16 @@
+'''
++------------------------------------------------------------------+
+| Title      : Personal GPT                                        |
+| Author     : Pranav Chiploonkar (@praetor29)                     |
+| Date       : 17 July 2023                                        |
++------------------------------------------------------------------+
+| Description: This program runs the core loop of the discord bot. |
++------------------------------------------------------------------+
+'''
+
+
+# Import functions and constants
+from constants import *
+from utility import *
+
+print("Hello")
