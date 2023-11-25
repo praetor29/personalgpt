694c952a63ec3187f2b09b6b4c5727e2bb6fb028
praetor29 <pranavchip@gmail.com>
2023-11-25 14:57:54 -0700

    upgraded pycord to py-cord-dev 2.5.0rc5

--- src/__main__.py
+++ src/__main__.py
@@ -2,11 +2,6 @@
 Project    : The PersonalGPT Project
 Author     : Pranav Chiploonkar (@praetor29)
 Description: Entry point to the program.
-
-Libraries:
-- python 3.10
-- py-cord 2.4.1
-- openai 1.3.3
 '''
 '''
 ██████╗ ███████╗██████╗ ███████╗ ██████╗ ███╗   ██╗ █████╗ ██╗      ██████╗ ██████╗ ████████╗
