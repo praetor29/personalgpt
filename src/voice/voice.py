45085d4697b090f9a01eb5216e93c6509be16944
Pranav Chiploonkar <70471259+praetor29@users.noreply.github.com>
2023-11-27 04:05:15 -0700

    working cleanup() -> next: get rid of dict

--- src/voice/voice.py
+++ src/voice/voice.py
@@ -45,55 +45,40 @@ class Voice(commands.Cog):
         """
         Verifies if the ctx is eligible.
         """
-        print("verify() called")
-        try: # for debugging!!
-            # 1. Reject non-guild requests (DMs)
-            if ctx.guild is None:
-                await ctx.respond('i can only vc in servers', ephemeral=True)
-                return False
-
-            # 2. Reject if not in VC
-            elif ctx.author.voice is None:
-                await ctx.respond('you have to join vc first', ephemeral=True)
-                return False
-            
-            # 3. Reject if already in VC
-            elif self.connections.get(ctx.guild.id, None) is not None:
-                await ctx.respond('im already in vc', ephemeral=True)
-                return False
-            
-            # (!) Handle potential mismatch
-            elif not await self.mismatch(ctx):
-                return False
-            
-            # Passed! Return confirmation
-            else:
-                await ctx.respond('ok ready now', ephemeral=True)
-                print('Passed verification!')
-                return True
-        except Exception as e:
-            print(f"Exception in verify(): {e}")
+        # 1. Reject non-guild requests (DMs)
+        if ctx.guild is None:
+            await ctx.respond('i can only vc in servers', ephemeral=True)
             return False
+
+        # 2. Reject if not in VC
+        elif ctx.author.voice is None:
+            await ctx.respond('you have to join vc first', ephemeral=True)
+            return False
+        
+        # 3. Reject if already in VC
+        elif self.connections.get(ctx.guild.id, None) is not None:
+            await ctx.respond('im already in vc', ephemeral=True)
+            return False
+                    
+        # Passed! Return confirmation
+        else:
+            await ctx.respond('ok ready now', ephemeral=True)
+            return True
 
     async def connect(self, ctx):
         """
         Connects to voice channel.
         """
-        # Debug print
-        print("connect() called")
-        print(f"Dictionary state: {self.connections.get(ctx.guild.id)}")
-        print(f"Discord.py state: {ctx.guild.voice_client}")
-        
-        try: # for debuggin!!
-            # Run verification
-            if await self.verify(ctx):
+        # Run verification
+        if await self.verify(ctx):
 
-                # Obtain channel
-                channel = ctx.author.voice.channel
-                # Connect to channel
+            # Obtain channel
+            channel = ctx.author.voice.channel
+            # Connect to channel
+            try:
                 await channel.connect()
-        except Exception as e:
-            print(f"Exception in connect(): {e}")
+            except Exception as e:
+                await ctx.respond(f"i think it broke: `{e}`", ephemeral=True)
 
 
     @discord.slash_command()
@@ -106,58 +91,45 @@ class Voice(commands.Cog):
     '''
     Utility Methods
     '''
-    
-    async def mismatch(self, ctx) -> bool:
-        """
-        Handles library vs dict state mismatch.
-        
-        Note: Something of a workaround!
-        """
-        print("mismatch() called")
-        if (
-            ctx.guild.voice_client is not None and
-            self.connections.get(ctx.guild.id, None) is None
-            ):
-            print("Mismatch detected")
-            try:
-                await ctx.guild.voice_client.disconnect()
-                print("Bot disconnected successfully")
-                ctx.guild.voice_client.cleanup()
-                print(f"Internal state after cleanup: {ctx.guild.voice_client}")
-                await asyncio.sleep(1)
-                print(f"Internal state after sleep: {ctx.guild.voice_client}")
-                return True
-            except Exception as e:
-                await ctx.respond(f'i think it broke: `{e}`', ephemeral=True)
-                print(f"Exception in mismatch(): {e}")
-                return False
-        print("No mismatch detected")
-        return True
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
+        """
+        Maintains the connections dictionary and handles state mismatches (zombie channels.)
 
+        Ensures voice channel connections are robust.
+        """
         # Check if member was bot
         if member == self.bot.user:
             
-            # If change was a disconnect
+            # If change was a *disconnect*
             if (
                 before.channel is not None and # was in VC
                 after.channel is None # no longer in VC
                 ):
-                # Remove connection from dict
+
+                # 1. Remove connection from dict
                 self.connections.pop(member.guild.id, None)
 
-                print(f"Updated dictionary state: removed the entry")
+                # 2. Cleanup potential zombie channels
+                if (
+                    member.guild.voice_client is not None and
+                    self.connections.get(member.guild.id, None) is None
+                    ):
+                    # First disconnect the client from zombie channel
+                    try:
+                        await member.guild.voice_client.disconnect()
+                    except Exception as e:
+                        print(f'Could not disconnect from zombie channel: {e}')
+                    # Then cleanup() zombie channel
+                    try:
+                        member.guild.voice_client.cleanup()
+                    except Exception as e:
+                        print(f'Could not cleanup zombie channel: {e}')
             
-            # If change was a connect
+            # If change was a *connect*
             elif (
                 after.channel is not None # is now in VC/different VC
                 ):
                 # Add connection to dict
                 self.connections[member.guild.id] = after.channel
-                print(f"Updated dictionary state: {self.connections.get(member.guild.id)}")
-
-
-                
-
